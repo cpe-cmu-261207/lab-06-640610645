18249470504dda5d4c9a201ddc3e3635370d28c8
640610645 <nontapan_chanadee@cmu.ac.th>
2022-08-11 18:08:19 +0700

    add UserCardDetail + make UserCard

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+  IconChevronDown,
+  IconChevronUp,
+  IconMailForward,
+  IconMapPins,
+} from "@tabler/icons";
 
-export default function UserCard() {
-  return <div>UserCard</div>;
+export default function UserCard(props) {
+  return (
+    <div>
+      <div className="border-bottom">
+        <div className="d-flex align-items-center p-3">
+          <img src="{props.imgUrl}" width="90px" class="rounded-circle me-4" />
+          <span className="text-center display-6 me-auto">{props.name}</span>
+          <IconChevronDown />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/UserCardDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserCardDetail() {
+  return (
+    <div>UserCardDetail</div>
+  )
+}
