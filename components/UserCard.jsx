9a487f8ebaa5fe0244e30fe3528cb0b111c2f7c3
640610645 <nontapan_chanadee@cmu.ac.th>
2022-08-11 18:13:15 +0700

    edit img src

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -11,7 +11,7 @@ export default function UserCard(props) {
     <div>
       <div className="border-bottom">
         <div className="d-flex align-items-center p-3">
-          <img src="{props.imgUrl}" width="90px" class="rounded-circle me-4" />
+          <img src={props.imgUrl} width="90px" class="rounded-circle me-4" />
           <span className="text-center display-6 me-auto">{props.name}</span>
           <IconChevronDown />
         </div>
